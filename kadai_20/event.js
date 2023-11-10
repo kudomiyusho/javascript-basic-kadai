47f4527c35e3636d1c7ae87605aeea5eb7fb4b1a
kudomiyusho <148519509+kudomiyusho@users.noreply.github.com>
2023-11-10 16:04:20 +0900

    javascript課題20をアップロード

---
+++ kadai_20/event.js
@@ -0,0 +1,7 @@
+const change = document.getElementById('btn');
+const text = document.getElementById('text');
+
+
+change.addEventListener('click', () => {
+    text.textContent ='ボタンがクリックされました';
+});
